2286b0bd70b5386a36f30ee4c3976d360c5a8b0b
John William Davis <jwdavisdeveloper@gmail.com>
2024-01-04 22:33:34 -0800

    add some custom fonts with fontsource and begin working out presentational components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./src/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Mukta', 'sans-serif', ...defaultTheme.fontFamily.sans],
+        serif: ['Lora Variable', 'serif', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 };
